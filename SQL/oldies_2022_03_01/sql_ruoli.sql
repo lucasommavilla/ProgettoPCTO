83be662284c9ac29f67f57edf2afa335e8d705bb
Luca Sommavilla <lucasomma7@gmail.com>
2023-11-29 14:27:35 +0100

    Delete sql_ruoli.sql

--- SQL/oldies_2022_03_01/sql_ruoli.sql
+++
@@ -1,4 +0,0 @@
-INSERT INTO `ruolo`(`id`, `dsruolo`) VALUES (1,'Docente');
-INSERT INTO `ruolo`(`id`, `dsruolo`) VALUES (2,'Studente');
-INSERT INTO `ruolo`(`id`, `dsruolo`) VALUES (3,'Segretario');
-INSERT INTO `ruolo`(`id`, `dsruolo`) VALUES (4,'Tutor aziendale');
