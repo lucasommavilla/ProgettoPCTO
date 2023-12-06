a938442ae6944328024a11a56ab3b1795c27d937
Tommybordi <150517837+Tommybordi@users.noreply.github.com>
2023-12-06 14:03:57 +0100

    Update index.php

--- index.php
+++ index.php
@@ -17,7 +17,7 @@
 	
     <main class="col-md-9 ms-sm-auto col-lg-10 px-md-4">
       <div class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
-        <h1 class="h2">Gestionale PCTO</h1>
+        <h1 class="h2">Gestionale PCTOO</h1>
       </div>
 	  <div class="row justify-content-left">
 	  <div class="col-md-4 col-sm-6 mb-2">
